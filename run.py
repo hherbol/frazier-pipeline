d450c099bd7a347519ebec844e1500fc12e081e2
Henry Herbol <hherbol@gmail.com>
2016-10-12 13:14:09 -0400

    Update to run

--- run.py
+++ run.py
@@ -9,10 +9,10 @@ solvent = "acetone"
 print("RUNNING TEST BENCH FOR %s in %s" % (solute, solvent))
 for i in range(1,10):
 	t0 = time.time()
-	e_solv = fpl_auto.get_enthalpy_solvation(solute,solvent,num_solvents=i,on_queue=True,unit="Ha",charge_and_multiplicity="2 1",charge_and_multiplicity_solvent="2 1",charge_and_multiplicity_solute="2 1")
+	e_solv = fpl_auto.get_enthalpy_solvation(solute,solvent,num_solvents=i,on_queue=True,unit="Ha",charge_and_multiplicity="2 1",charge_and_multiplicity_solvent="0 1",charge_and_multiplicity_solute="2 1",name_append="_%d"%i,route_lvls=[0,0,0,0])
 	e_solv.wait()
 	H = e_solv.enthalpy()
 	t = time.time()
-	print("Enthalpy of Solvation for pb2+ in 10 acetone is %lg Ha = %lg kcal/mol" % (H, units.convert_energy("Ha","kcal/mol",H)))
+	print("Enthalpy of Solvation for pb2+ in %d acetone is %lg Ha = %lg kcal/mol" % (i, H, units.convert_energy("Ha","kcal/mol",H)))
 	print("\tNum Solvents = %d, time to complete = %lg s" % (i, t-t0))
 	sys.stdout.flush()

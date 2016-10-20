7fd6b895fdea5fb4b31056b896c7f4c38d7fba4f
Henry Herbol <hherbol@gmail.com>
2016-10-20 10:12:05 -0400

    Test case

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import fpl_auto
 
-umbo = fpl_auto.get_UMBO(["Cl","I","I"],"MA","acetone")
-print("UMBO for acetone on PbClI2MA = %lg" % umbo)
+umbo = fpl_auto.get_UMBO(["Br","I","I"],"FA","acetone")
+print("UMBO for acetone on PbBrI2FA = %lg" % umbo)
